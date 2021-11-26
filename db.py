5feabd1987cc9fe944943e312a3b4aaebf295a52
Dan Sazonov <p.294803@gmail.com>
2021-11-26 23:17:17 +0300

    Подключил бд, написал иниты

--- db.py
+++ db.py
@@ -10,11 +10,28 @@ class Main:
     """
     Основная бд с инфой о подписчиках
     """
-    pass
+    def __init__(self):
+        db = psycopg2.connect(config.DB_URI, sslmode='require')
+        cursor = db.cursor()
+        self.db, self.cursor = db, cursor
+
+        cursor.execute("CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, username TEXT, join_date TIMESTAMP, "
+                       "messages INTEGER)")
+        db.commit()
+        db.close()
 
 
 class Santa:
     """
     Бд с инфой по тайному санте
     """
-    pass
+
+    def __init__(self):
+        db = psycopg2.connect(config.DB_URI, sslmode='require')
+        cursor = db.cursor()
+        self.db, self.cursor = db, cursor
+
+        cursor.execute("CREATE TABLE IF NOT EXISTS santa(id INTEGER PRIMARY KEY, username TEXT, wishes TEXT, "
+                       "address TEXT, on_meeting BOOLEAN)")
+        db.commit()
+        db.close()
