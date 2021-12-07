adb1ea1d921a6260cc63093f33818ea5d59a3e28
Dan Sazonov <p.294803@gmail.com>
2021-12-08 01:15:52 +0300

    Запилил рандомайзер

--- drawing.py
+++ drawing.py
@@ -4,5 +4,20 @@ import random
 db = Santa()
 
 
-def get_pairs():
+def get_pairs() -> list[tuple[int, int]]:
+    """
+    Возвращает список кортежей с парами санта - подопечный
+
+    :return: [(санта, подопечный), ]
+    """
+
     players = db.get_players()
+    # players = [1, 2, 3, 4, 5]  # для дебага
+    random.shuffle(players)
+
+    pairs = [(players[-1], players[0])]
+    for i in range(len(players)-1):
+        pairs.append((players[i], players[i+1]))
+
+    return pairs
+
