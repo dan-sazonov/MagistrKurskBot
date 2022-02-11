5181fc401f2ddc587bcd8f2f63e9f0c6089c7ac5
Dan Sazonov <p.294803@gmail.com>
2022-02-11 23:08:15 +0300

    Перенес апдейт счетика в post_process

--- middlewares.py
+++ middlewares.py
@@ -7,5 +7,5 @@ db_main = db.Main()
 
 
 class Middle(BaseMiddleware):
-    async def on_process_message(self, message: types.Message, data: dict):
+    async def on_post_process_message(self, message: types.Message, data_from_filter: list, data: dict):
         db_main.update_counter(message.from_user.id, str(message.text).lstrip('/'))
