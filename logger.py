da6ef826691e408f7238556b8672307b41da40be
Dan Sazonov <p.294803@gmail.com>
2022-02-12 02:51:26 +0300

    Логгирование новых апдейтов

--- logger.py
+++ logger.py
@@ -4,7 +4,7 @@ import os
 _log_format = '%(asctime)s [%(levelname)s] - %(name)s(%(filename)s, %(lineno)d): %(message)s'
 
 
-def _safe_mkdir(path: str) -> None:
+def _safe_mkfile(path: str) -> None:
     if not os.path.exists('./logs'):
         os.mkdir('./logs')
     if not os.path.exists(f'./logs/{path}'):
@@ -12,12 +12,12 @@ def _safe_mkdir(path: str) -> None:
 
 
 def set_basic_logger():
-    _safe_mkdir('main.log')
+    _safe_mkfile('main.log')
     logging.basicConfig(level=logging.INFO, filename='./logs/main.log', filemode='w+', format=_log_format)
 
 
 def get_file_handler():
-    _safe_mkdir('warnings.log')
+    _safe_mkfile('warnings.log')
     file_handler = logging.FileHandler('./logs/warnings.log')
     file_handler.setLevel(logging.WARNING)
     file_handler.setFormatter(logging.Formatter(_log_format))
@@ -36,4 +36,15 @@ def get_logger(name):
     logger.setLevel(logging.INFO)
     logger.addHandler(get_file_handler())
     logger.addHandler(get_stream_handler())
+    return logger
+
+
+def get_updates_logger():
+    file_handler = logging.FileHandler('./logs/updates.log')
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s: %(message)s'))
+
+    logger = logging.getLogger('get_updates')
+    logger.setLevel(logging.INFO)
+    logger.addHandler(file_handler)
     return logger
