199403472d0cbf5e3f266765eeaeec8d5816a1a1
Dan Sazonov <p.294803@gmail.com>
2022-02-15 02:28:06 +0300

    Восстановил логгер

--- dispatcher.py
+++ dispatcher.py
@@ -9,11 +9,10 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import config
 import filters
 import logger
-import logging
-# from middlewares import Middle
+from middlewares import Middle
 
 # логирование
-logging.basicConfig(level=logging.INFO)
+logger.set_basic_logger()
 
 # инит
 TOKEN = config.API_TOKEN
@@ -21,5 +20,5 @@ bot = aiogram.Bot(token=TOKEN, parse_mode="HTML")
 storage = MemoryStorage()
 
 dp = aiogram.Dispatcher(bot, storage=storage)
-# dp.middleware.setup(Middle())
+dp.middleware.setup(Middle())
 dp.bind_filter(filters.IsAdmin)
